c44976cc671ab0200ed169f61de946e34c5bcd0a
ILikeCodingg5565 <136778324+ILikeCodingg5565@users.noreply.github.com>
2025-04-22 09:13:25 +1000

    Create my-script.user.js

---
+++ my-script.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         My Extension Script
+// @version      1.0
+// @description  Script created from Chrome Extension
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+console.log("Hello from a Tampermonkey script!");
